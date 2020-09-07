30628301e48903e9dc509b3f7ea737576b9a822c
pompom poto <48266828+jjoohee@users.noreply.github.com>
2020-09-08 05:55:29 +0900

    Update voice-js.js

--- voice-js.js
+++ voice-js.js
@@ -60,7 +60,7 @@ window.onscroll = function(){
         
      if (Math.round( $(window).scrollTop()) == $(document).height() - $(window).height()) {
         
-       // var url = "https://jjoohee.github.io/doong0826/";
+ 	 var url = "https://jjoohee.github.io/misfortune/";
 		$(location).attr('href',url);
         
     }
