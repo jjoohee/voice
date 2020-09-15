c48185e0bb07b5900608e85139386dea179d692d
pompom poto <48266828+jjoohee@users.noreply.github.com>
2020-09-15 13:12:21 +0900

    Update voice-js.js

--- voice-js.js
+++ voice-js.js
@@ -1,47 +1,43 @@
-//목소리
-person1.onclick = function() {
-	person1.style.opacity = "0%";
-	person2.style.opacity = "100%";
-	smoke.style.opacity = "100%";
-}
-
-
-person2.onclick = function() {
-	person2.style.opacity = "0%";
-	person1.style.display = "none";
-	smoke.style.opacity = "0%";
-	smoke.style.animation = "smo 4s 0.5s forwards";
-}
-
-
 //스크롤 작용
 window.onscroll = function(){
 		var t =document.documentElement.scrollTop + document.body.scrollTop;
 		moniter.innerHTML = t
 
-		if (t > 700) {
+		if (t > 3300) {
 			mask.style.left = "-2px";
+
 
 		}
 		else{
 			mask.style.left = "";
 		}
 
-		if (t > 1100) {
+		if (t > 3500) {
 			mask_2.style.right = "-2px";
+			voicemain.style.background = "url(back0-2.gif)";
 
 		}
 		else{
 			mask_2.style.right = "";
+			voicemain.style.background = "";
 		}
 
 
-		if (t > 1600) {
+		if (t > 3700) {
 			mask_3.style.left = "-2px";
 
 		}
 	else{
 			mask_3.style.left = "";
+		}
+
+
+		if (t > 5900) {
+			mask3.style.opacity = "100%";
+
+		}
+	else{
+			mask3.style.opacity = "";
 		}
 
 }
